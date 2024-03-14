456e7d9f702627a88b1a0df5ada4fd290192fbe3
ubuntu <ubuntu@Ubuntu.myguest.virtualbox.org>
2024-03-14 15:41:34 +0000

    All targets sh file created

---
+++ Assignment1/run_all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Compile and run daemon
+make run-daemon
+
+# Compile and run backup
+make run-backup
+
+# Compile and run reports
+make run-reports
+
+# Compile and run lock
+make run-lock
+
+# Compile and run unlock
+make run-unlock
+
+# Compile and run check
+make run-check
+
+# Compile and run collect
+make run-collect
